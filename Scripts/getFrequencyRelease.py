e8163cad062207c5cede2ffdd16dcfcc1ad88887
Lorenagoc <lorenagoc99@gmail.com>
2023-09-19 23:09:25 -0300

    Release Frequency - OK

--- Scripts/getFrequencyRelease.py
+++ Scripts/getFrequencyRelease.py
@@ -1,6 +1,11 @@
 from github import Github
 import json
 
+def send_average_to_file(output_file, keyword, num_found):
+    output_file = open(output_file, "a")
+    output_file.write(keyword + ":" + str(num_found) + "\n")
+    output_file.close()
+
 def get_artifacts():
     artifacts = {}
     with open("Dataset.json", "r") as myfile:
@@ -15,8 +20,14 @@ def get_frequency_release(repository_name):
     # Obtém o repositório desejado
     repo = g.get_repo(repository_name)
 
+    # Obtém as releases do repositório
     releases = repo.get_releases()
+
+    # Obtém o número de releases
     num_releases = releases.totalCount
+
+    # Lista para armazenar as datas de lançamento
+    dates = []
 
     if num_releases >= 2:
         # Obtém as duas últimas releases
@@ -25,15 +36,15 @@ def get_frequency_release(repository_name):
         # Itera pelas releases e imprime informações relevantes
         print("Releases do repositório:", repository_name)
         for release in releases:
-            # print("Nome:", release.title)
-            # print("Tag:", release.tag_name)
+            dates.append(release.created_at)
             print("Data de lançamento:", release.created_at)
+        average = abs((dates[0] - dates[1]))
+        print("Média de dias entre releases:", average)
         print("\n")
     elif num_releases == 1:
         print("Última release do repositório:", repository_name)
         release = repo.get_latest_release()
-        # print("Nome:", release.title)
-        # print("Tag:", release.tag_name)
+        average = 0
         print("Data de lançamento:", release.created_at)
         print("\n")
     else:
@@ -41,15 +52,20 @@ def get_frequency_release(repository_name):
         print("Não há releases para o repositório:", repository_name)
         for commit in last_commits:
             commit_created_at = commit.commit.author.date
+            dates.append(commit_created_at)
             print("Data de lançamento do commit:", commit_created_at)
+        average = abs((dates[0] - dates[1]))
+        print("Média de dias entre commits:", average)
         print("\n")
+    return average
 
 
 def get_repository_name():
     artifacts = get_artifacts()
     for artifact in artifacts:
         repository_name = artifact["FullRepoName"]
-        get_frequency_release(repository_name)
+        average = get_frequency_release(repository_name)
+        send_average_to_file("AverageFrequencyRelease.txt", repository_name, average)
 
 if __name__ == "__main__":
     get_repository_name()
