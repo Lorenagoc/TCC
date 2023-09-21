8c32f568b22267178ed9e1e102fea1eefd6dfc63
Lorenagoc <lorenagoc99@gmail.com>
2023-09-20 23:25:20 -0300

    Last Discussions Stack Overflow - OK

--- Scripts/getLastDicussionSO.py
+++ Scripts/getLastDicussionSO.py
@@ -1,27 +1,17 @@
 from stackapi import StackAPI
+import datetime
 import json
+
+def send_last_discussions_to_file(output_file, keyword, num_found):
+    output_file = open(output_file, "a")
+    output_file.write(keyword + ":" + str(num_found) + "\n")
+    output_file.close()
 
 def get_artifacts():
     artifacts = {}
     with open("Dataset.json", "r") as myfile:
         artifacts = json.loads(myfile.read(), strict=False)
     return artifacts
-   
-
-def getLastDiscussedDates(tag):
-   # Crie uma instância da StackAPI
-    user_api_key = 'hDxbIGl*XQDx7qfyHHi9QA(('
-    so = StackAPI('stackoverflow', key=user_api_key, impose_throttling=True)
-
-    questions = so.fetch('questions', tagged=[tag, 'java'], sort='creation', order='desc', limit=5)
-    dates_string = ""
-    for i in range(0, 10):
-        if questions == None or i >= len(questions):
-            break
-        if i > 0:
-            dates_string += ';'
-        dates_string += questions[i].creation_date.strftime("%m/%d/%Y, %H:%M:%S") + " UTC"
-    print("Datas: ", dates_string)
 
 def get_so_tag():
     artifacts = get_artifacts()
@@ -30,13 +20,35 @@ def get_so_tag():
         so_tag = artifact["SOtags"]
         tags.append(so_tag)
     print("\n")
-    return tags
-        
-  
+    return tags   
+
+def getLastDiscussedDates(tag):
+   # Crie uma instância da StackAPI
+    user_api_key = 'hDxbIGl*XQDx7qfyHHi9QA(('
+    so = StackAPI('stackoverflow', key=user_api_key, impose_throttling=True)
+
+    # Obtem as cinco perguntas mais recentes com a tag
+    questions = so.fetch('questions', tagged=[tag, 'java'], sort='creation', order='desc', limit=5)
+    dates_string = ""
+
+    if questions['items']:
+        for i, question in enumerate(questions['items'], start=1):
+            creation_date_formatted = datetime.datetime.fromtimestamp(question['creation_date']) .strftime('%d-%m-%Y %H:%M:%S')
+
+            if i >= len(questions):
+                dates_string += creation_date_formatted
+                break
+            elif i > 0:
+                dates_string += creation_date_formatted + ";"
+    else:
+        dates_string = None
+    return dates_string
 
 if __name__ == "__main__":
   tags = get_so_tag()
-  print ("Tags: ", tags)
   for tag in tags:
-    print("Tag: ", tag)
-    getLastDiscussedDates(tag)
+    print("Tag:", tag)
+    last_discussions = getLastDiscussedDates(tag)
+    print("Últimas discussões:", last_discussions)
+    print("\n")
+    send_last_discussions_to_file("last_discussions.txt", tag, last_discussions)
